098116ed615455d423df4734cc9a71bb2c20d85a
hammadulmustafa-albarvi <hammadulmustafa2007@gmail.com>
2026-02-06 22:12:13 +0500

    Add files via upload

---
+++ Spam classifier/app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pickle
+model = pickle.load(open("D:/Campus X/Spam classifier/spam_predition.pkl", "rb"))
+vectorizer = pickle.load(open("D:/Campus X/Spam classifier/vectorizer.pkl", "rb"))
+st.header("Applications of Naive Bayes ")
+st.title("Spam Classifier")
+text = st.text_input("Enter text: ")
+if text:
+    text = vectorizer.transform([text])
+    prediction = model.predict(text)
+    if prediction == 1:
+        st.write("Spam")
+    else:
+        st.write("Ham")
+        
+        
+st.title("Movie Sentiment Analysis")
+sentiment =    st.text_input("Enter movie review : ")
+m = pickle.load(open("D:/Campus X/Spam classifier/model.pkl", "rb"))
+vc = pickle.load(open("D:/Campus X/Spam classifier/vectoriz.pkl", "rb"))
+if sentiment:
+    sentiment = vc.transform([sentiment])
+    pred = m.predict(sentiment)
+    if pred == 1:
+         st.write("Positive")
+    else:
+        st.write("Negative")
